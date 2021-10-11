eec698b4d955ca6979337cde0d4de597b57bda56
jschoi999 <73480104+jschoi999@users.noreply.github.com>
2021-10-11 17:05:19 +0900

    Create  ContactNumber.java
    프로그래머스 전화번호 목록 문제

---
+++ ContactNumber.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        for(int i=1; i<phone_book.length;i++){
+            
+            for(int j=0; j<phone_book.length; j++){
+                if(i==j){
+                    continue;
+                }
+                if(phone_book[i].indexOf(phone_book[j]) == 0 || phone_book[j].indexOf(phone_book[i]) == 0){
+                    answer = false;
+                    break;
+                }
+                
+            }
+            
+        }
+        
+        return answer;
+    }
+}
